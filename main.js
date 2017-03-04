7bd4bac328e272f2c3ca9d1049466269f73d421c
turunco <yumi.kataoka.2015@gmail.com>
2017-03-04 01:13:12 -0800

    use sandbox

--- main.js
+++ main.js
@@ -58,13 +58,64 @@ console.log(callback.calledWith(1, 2, 3));
 // Stub
 //-----------------------------------
 // test function
-function once2(func) {
-    return 5;
-}
-
 var callback2 = sinon.stub().returns(42);
 var proxy2 = once(callback2);
 
 if (proxy2() === 42) {
     console.log('proxy2() == 42');
 }
+
+
+//===================================
+// sandbox
+//===================================
+class Klass {
+    constructor() {
+        console.log('Klass.constructor');
+    }
+
+    func(args) {
+        return args;
+    }
+
+    func2(args) {
+        return args;
+    }
+};
+
+//-----------------------------------
+// Spy
+//-----------------------------------
+global.sandbox = sinon.sandbox.create(); // create sandbox
+var klass_ = new Klass();
+
+sandbox.spy(klass_, 'func');
+klass_.func();
+console.log('sandbox-spy', klass_.func.calledOnce);
+
+sandbox.restore(); // restore sandbox
+
+//-----------------------------------
+// Stub
+//-----------------------------------
+global.sandbox = sinon.sandbox.create(); // create sandbox
+var klass_ = new Klass();
+
+sandbox.stub(klass_, 'func', () => {
+    console.log('sandbox-stub-func');
+});
+klass_.func();
+// sinon.assert.calledOnce(klass_.func);
+console.log('sandbox-stub-func', klass_.func.called);
+
+sandbox.stub(klass_, 'func2', (args) => {
+    console.log('sandbox-stub-func2');
+    const { param0, param1 } = JSON.parse(args);
+    console.log('sandbox-stub-func2 - 0');
+    console.log(param0 === 'param0' ? 'param0 === \'param0\'' : 'param0 !== \'param0\'');
+    console.log(param1 === 'param1' ? 'param1 === \'param1\'' : 'param1 !== \'param1\'');
+});
+klass_.func2(JSON.stringify({ param0: 'param0', param1: 'paramX' }));
+console.log('sandbox-stub-func2', klass_.func2.called);
+
+sandbox.restore(); // restore sandbox
